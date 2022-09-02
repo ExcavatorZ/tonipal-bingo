272592e120939385fb76173f467b84824408fa5e
ExcavatorZ <taneli.murtola@turku.fi>
2022-09-02 13:45:26 +0300

    A very poor attempt at adding a checkmark.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import './index.css';
+import Checkmark from './components/Checkmark';
 
 function App() {
   return (
     <div className="App">
       <h1>Tonipal Bingo</h1>
       <section id="table">
-        <div class="box a">Kahvi</div>
-        <div class="box b">Energiajuoma</div>
-        <div class="box c">Kuohuvesi</div>
-        <div class="box d">Leipä</div>
-        <div class="box e">Nyssykkä</div>
-        <div class="box f">Patukka</div>
-        <div class="box g">Mikroateria</div>
-        <div class="box h">Sushi</div>
-        <div class="box i">Palautusjuoma</div>
+        
+        <div class="box a">Kahvi<Checkmark /></div>
+        <div class="box b">Energiajuoma<Checkmark /></div>
+        <div class="box c">Kuohuvesi<Checkmark /></div>
+        <div class="box d">Leipä<Checkmark /></div>
+        <div class="box e">Nyssykkä<h1>X</h1></div>
+        <div class="box f">Patukka<Checkmark /></div>
+        <div class="box g">Mikroateria<Checkmark /></div>
+        <div class="box h">Sushi<Checkmark /></div>
+        <div class="box i">Palautusjuoma<Checkmark /></div>
       </section>
     </div>
   );
---
+++ src/components/Checkmark.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import "../index.css";
+
+const Checkmark = () => {
+    const [mark, setMark] = useState('');
+
+    const handleClick = () => {
+        if (mark === '') {
+            setMark('X');
+        } else {
+            setMark('');
+        }
+    };
+    return (
+        <>
+        <h1>{mark}</h1>
+        <div onClick={handleClick} id="invisibletext">
+            asdasdasdasdasdasdasd
+            asdasdasdasdasdasdasd
+            asdasdasdasdasdasdasd
+        </div>
+        </>
+    );
+};
+
+export default Checkmark
