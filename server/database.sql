3327eebd5e4af638c5eb2bc4173ab5deb319721e
ExcavatorZ <taneli.murtola@turku.fi>
2022-11-17 15:10:06 +0200

    Not null constraint.

--- server/database.sql
+++ server/database.sql
@@ -1,9 +1,13 @@
 CREATE DATABASE bingo;
 
+\c bingo /* connect to bingo */
+
 CREATE TABLE results (
     item_id SERIAL PRIMARY KEY,
     item VARCHAR(20) UNIQUE,
-    quantity INTEGER
+    quantity INTEGER NOT NULL
 );
 
-SELECT * FROM results;
+SELECT * FROM results; /* select all */
+
+DROP DATABASE bingo; /* delete database */
