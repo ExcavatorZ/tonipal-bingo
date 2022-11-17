515f62eabb046c316b5315959709b0d40ed68ba2
ExcavatorZ <taneli.murtola@turku.fi>
2022-11-17 15:50:44 +0200

    Leaderboard placeholder.

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,13 +1,28 @@
 import { useNavigate } from "react-router-dom"
+import { Fragment } from "react";
 
 export const Leaderboard = () => {
     const navigate = useNavigate();
 
     return (
-        <div>
+        <Fragment>
+            <table class="table alignitems-center text-center">
+                <thead>
+                    <tr>
+                        <th>Item</th>
+                        <th>Quantity</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Nyssykkä</td>
+                        <td>1</td>
+                    </tr>
+                </tbody>
+            </table>
             Leaderboard is supposed to come here.
             <br />
             <button onClick={() => navigate('/')} style={{margin: "20px"}} className="button">Back to bingo.</button>
-        </div>
+        </Fragment>
     )
 }
