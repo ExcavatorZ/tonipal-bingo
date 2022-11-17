828a2e2f515e8bed8f9edc9169dd7523f13c20a0
ExcavatorZ <taneli.murtola@turku.fi>
2022-11-17 08:04:06 +0200

    Revamping code and structure.

--- src/App.js
+++ src/App.js
@@ -3,32 +3,14 @@ import Checkmark from './components/Checkmark';
 import Randomizer from './components/Randomizer';
 
 function App() {
-  const itemList = Randomizer()
   
   return (
     <div className="App">
       <h1>Tonipal Bingo</h1>
-      <section id="table">
-        <div className="box a">{itemList[0]}<Checkmark /></div>
-        <div className="box b">{itemList[1]}<Checkmark /></div>
-        <div className="box c">{itemList[2]}<Checkmark /></div>
-        <div className="box d">{itemList[3]}<Checkmark /></div>
-        <div className="box e">{itemList[4]}<Checkmark /></div>
-        <div className="box f">{itemList[5]}<Checkmark /></div>
-        <div className="box g">{itemList[6]}<Checkmark /></div>
-        <div className="box h">{itemList[7]}<Checkmark /></div>
-        <div className="box i">{itemList[8]}<Checkmark /></div>
-        <div className="box j">{itemList[9]}<Checkmark /></div>
-        <div className="box k">{itemList[10]}<Checkmark /></div>
-        <div className="box l">{itemList[11]}<Checkmark /></div>
-        <div className="box m">{itemList[12]}<Checkmark /></div>
-        <div className="box n">{itemList[13]}<Checkmark /></div>
-        <div className="box o">{itemList[14]}<Checkmark /></div>
-        <div className="box p">{itemList[15]}<Checkmark /></div>
-      </section>
+        <Randomizer />
       <br/>
-      <button style={{marginLeft: "300px", float: "left"}} className="button">Submit</button>
-      <button style={{marginRight: "300px", float: "right"}} className="button">Leaderboards</button>
+      {/*<button style={{marginLeft: "300px", float: "left"}} className="button">Submit</button>
+      <button style={{marginRight: "300px", float: "right"}} className="button">Leaderboards</button>*/}
     </div>
   );
 }
--- src/components/Randomizer.js
+++ src/components/Randomizer.js
@@ -1,3 +1,5 @@
+import { Checkmark } from "./Checkmark";
+
 const bingoItems = ["Kahvi", "Energiajuoma", "Kuohuvesi", "Leipä", "Nyssykkä", "Patukka",
 "Mikroateria", "Sushi", "Palautusjuoma", "Banaani", "Joku rahka hömmeli", "Roiskeläppä",
 "Pähkinäpussi", "Energiavesi", "Tikkukaramelli", "Salaatti"];
@@ -7,7 +9,31 @@ const Randomizer = () => {
         .map((value) => ({ value, sort: Math.random() }))
         .sort((a, b) => a.sort - b.sort)
         .map(({ value }) => value);
-    return newBingo;
+    return (
+        <div>
+            <section id="table">
+                <div className="box a">{newBingo[0]}<Checkmark /></div>
+                <div className="box b">{newBingo[1]}<Checkmark /></div>
+                <div className="box c">{newBingo[2]}<Checkmark /></div>
+                <div className="box d">{newBingo[3]}<Checkmark /></div>
+                <div className="box e">{newBingo[4]}<Checkmark /></div>
+                <div className="box f">{newBingo[5]}<Checkmark /></div>
+                <div className="box g">{newBingo[6]}<Checkmark /></div>
+                <div className="box h">{newBingo[7]}<Checkmark /></div>
+                <div className="box i">{newBingo[8]}<Checkmark /></div>
+                <div className="box j">{newBingo[9]}<Checkmark /></div>
+                <div className="box k">{newBingo[10]}<Checkmark /></div>
+                <div className="box l">{newBingo[11]}<Checkmark /></div>
+                <div className="box m">{newBingo[12]}<Checkmark /></div>
+                <div className="box n">{newBingo[13]}<Checkmark /></div>
+                <div className="box o">{newBingo[14]}<Checkmark /></div>
+                <div className="box p">{newBingo[15]}<Checkmark /></div>
+            </section>
+            <br/>
+            <button style={{marginLeft: "630px", float: "left"}} className="button">Submit</button>
+            <button style={{marginRight: "630px", float: "right"}} className="button">Leaderboards</button>
+      </div>
+    );
     };
 
 export default Randomizer
