dc6325fde8cd74433875042790ba9de36bb7ee23
ExcavatorZ <excavatorz@hotmail.fi>
2023-01-24 17:34:31 +0200

    Added test configuration and simple homepage test.

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "airbnb",
+    // https://github.com/jsx-eslint/eslint-plugin-react/blob/577cb64fb6/README.md#configuration
+    "plugin:react/jsx-runtime",
+    "prettier",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+  },
+};
