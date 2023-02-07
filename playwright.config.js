93a0d5becee1ce888b2167eecc3495c0be601a4e
ExcavatorZ <taneli.murtola@turku.fi>
2023-02-07 15:55:26 +0200

    Trying port 3000.

--- playwright.config.js
+++ playwright.config.js
@@ -14,8 +14,8 @@ import { devices } from "@playwright/test";
  */
 const config = {
   webServer: {
-    command: "set PORT=3001 && react-scripts start",
-    port: 3001,
+    command: "react-scripts start",
+    port: 3000,
   },
 
   testDir: "./tests",
@@ -43,7 +43,7 @@ const config = {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "http://localhost:3001",
+    baseURL: "http://localhost:3000",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on",
